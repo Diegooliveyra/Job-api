c11c9e691d1ebc5e70d6eec378fdb26034297a7a
Diego Oliveira <diegooliveira@Diegos-MacBook-Air.local>
2023-08-06 19:03:19 -0300

    feat: add express intial config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello world')
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.get('/users', (req, res) => {
+  res.status(200).send(JSON.stringify([{ nome: 'Diego' }]));
+});
+
+app.listen(port, () => {
+  console.log(`Listenig on port ${port}!`);
+});
